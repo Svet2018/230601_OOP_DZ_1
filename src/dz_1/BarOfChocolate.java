755f4bfcfffcb996bee57dd75b401c7177c32335
Svet2018 <bulatova44@gmail.com>
2023-06-01 19:19:27 +0300

    Added some code

--- src/dz_1/BarOfChocolate.java
+++ src/dz_1/BarOfChocolate.java
@@ -3,15 +3,68 @@ package dz_1;
 public class BarOfChocolate extends Product{
 
     private double calorieContent; //Содержание каллорий
+    private String colorChocolate; //Цвет шоколада
+    private int massChocolate;
 
-    public BarOfChocolate (String brand, String name, double price, double calorieContent) {
+
+
+    public BarOfChocolate (String brand, String name, double price, double calorieContent, String colorChocolate, int massChocolate) {
         super(brand, name, price);
-        this.calorieContent = calorieContent;
+        checkCalories(calorieContent);
+        checkColor(colorChocolate);
+        checkMass(massChocolate);
+    }
+
+    public double getCalorieContent() {
+        return calorieContent;
+    }
+
+    public void setCalorieContent(double calorieContent) {
+        checkCalories(calorieContent);
+    }
+
+    public String getColorChocolate() {
+        return colorChocolate;
+    }
+
+    public void setColorChocolate(String colorChocolate) {
+        checkColor(colorChocolate);
+    }
+
+    public int getMassChocolate() {
+        return massChocolate;
+    }
+
+    public void setMassChocolate(int massChocolate) {
+        checkMass(massChocolate);
+    }
+
+    protected void checkMass (int massChocolate){
+        if (massChocolate < 50){
+            this.massChocolate = 50;
+        }
+        else this.massChocolate = massChocolate;
+    }
+
+    protected void checkCalories (double calorieContent){
+        if (calorieContent < 0){
+            this.calorieContent = 200;
+        }
+        else this.calorieContent = calorieContent;
+    }
+    protected void checkColor (String colorChocolate){
+        if (colorChocolate.equals("Белый") || colorChocolate.equals("Черный") || colorChocolate.equals("Молочный")){
+            this.colorChocolate = colorChocolate;
+        }
+        else if (colorChocolate.isEmpty() || colorChocolate.isBlank()) {
+            this.colorChocolate = "Черный";
+        }
+        else this.colorChocolate = "Черный";
     }
 
     @Override
     public String displayInfo() {
-        return String.format("Плитка шоколада: Производитель - %s; Название - %s; Цена - %f; Калории - %f", brand, name, price, calorieContent);
+        return String.format("Плитка шоколада: Производитель - %s; Название - %s; Цена - %f; Калории - %f; Цвет шоколадки - %s; Масса шоколадки (гр) - %d", brand, name, price, calorieContent, colorChocolate, massChocolate);
     }
 
 
--- src/dz_1/Main.java
+++ src/dz_1/Main.java
@@ -14,12 +14,14 @@ public class Main {
         System.out.println("Товары на продажу:");
         Product bottleOfWater1 = new BottleOfWater("ООО Чистый источник", "Бутылка с водой [1]", 115, 1);
         Product bottleOfWater2 = new BottleOfWater("ООО Чистый источник", "Бутылка с водой [2]", 215, 1.5);
-
-        BottleOfWater bottleOfWater3 = new BottleOfWater("ООО Чистый источник", "Бутылка с водой [3]", 1115, 1.5);
+        Product bottleOfWater3 = new BottleOfWater("ООО Чистый источник", "Бутылка с водой [3]", 1115, 1.5);
         Product bottleOfWater4 = new BottleOfWater("ООО Чистый источник", "Бутылка с водой [4]", 55, 2);
         Product bottleOfMilk1 = new BottleOfMilk("ООО Молоко натуральное", "Бутылка с молоком", 115, 1.5, 10);
-        Product barOfChocolate1 = new BarOfChocolate("ООО Сладость", "Шоколадка вкусная", 98, 220);
-        Product barOfChocolate2 = new BarOfChocolate("ООО Вкус", "Шоколад белый", 128, 120);
+        Product barOfChocolate1 = new BarOfChocolate("ООО Сладость", "Шоколадка вкусная", 98, 220, "Черный", 30);
+        Product barOfChocolate2 = new BarOfChocolate("ООО Вкус", "Шоколад белый", 128, -120, "Белый", 150);
+        Product barOfChocolate3 = new BarOfChocolate("ООО Вкус", "Шоколадка", 132, 320, "Молочный", 90);
+        Product barOfChocolate4 = new BarOfChocolate("ООО Сладость", "Солнышко", 332, 202, " ", 240);
+        Product barOfChocolate5 = new BarOfChocolate("ООО Шоколад", "Аленка", -1, 402, "чч", 55);
 
         List<Product> products = new ArrayList<>();
         products.add(bottleOfWater1);
@@ -29,6 +31,9 @@ public class Main {
         products.add(bottleOfWater3);
         products.add(barOfChocolate1);
         products.add(barOfChocolate2);
+        products.add(barOfChocolate3);
+        products.add(barOfChocolate4);
+        products.add(barOfChocolate5);
 
         for (int i = 0; i < products.size(); i++){
             System.out.println(products.get(i).displayInfo());
